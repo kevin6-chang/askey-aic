959d5d8b0c555ca1489c0bf5a3ff9a4618aa1543
Kevin Chang <kevin6_chang@askey.com>
2026-02-19 14:28:34 -0800

    added more script

---
+++ scripts/clone_tapei_security_ncwbr_build_git.sh
@@ -0,0 +1,2 @@
+#git clone ssh://git@10.1.6.85:7999/projects/NCWBR/repos/build/browse/cbe1v1k/askey_code/askey_pub/askey_secure_key?at=refs%2Fheads%2FNCQ2500D3_spf12.2csu2_V0.6
+git clone ssh://git@10.1.6.85:7999/ncwbr/build.git
